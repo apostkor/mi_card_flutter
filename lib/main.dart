21f586eaaa3b6e0d38143266c6263f7a9889e1ad
어포스트 (apost) <45118109+apostkor@users.noreply.github.com>
2020-02-12 01:16:50 +0900

    app done

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,109 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/profile.png'),
+            ),
+            Text(
+              'Jessica Jeon',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'PRESCHOOL TEACHER',
+              style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+                height: 20.0,
+                width: 120.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                )),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 25,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+63 677 644 0345',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro'),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 25,
+              ),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'patty5361654@gmail.com',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro'),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
+//Row(
+//children: <Widget>[
+//Icon(
+//Icons.phone,
+//color: Colors.teal,
+//),
+//SizedBox(
+//width: 10,
+//),
+//Text(
+//'+63 977 644 0345',
+//style: TextStyle(
+//fontFamily: 'SourceSansPro',
+//fontSize: 20,
+//color: Colors.teal.shade900,
+//),
+//)
+//],
+//),
